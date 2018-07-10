69cce01f9f2fcb049ceb5c4026344c0c08dfac2f
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-07-10 10:11:01 +0200

    Added all symbols in __all__

--- yamlable/__init__.py
+++ yamlable/__init__.py
@@ -3,6 +3,9 @@ from yamlable.main import YamlCodec, register_yamlable_codec, yaml_info_decorate
     AbstractYamlAble, YAMLABLE_PREFIX
 from yamlable.yaml_objects import YamlObject2, ABCYAMLMeta, YAMLObjectMetaclassStrict
 
-__all__ = ['base',
-           'main',
-           'yaml_objects']
+__all__ = ['base', 'main', 'yaml_objects',
+           'AbstractYamlObject', 'NONE_IGNORE_CHECKS', 'Y', 'read_yaml_node_as_dict',
+           'YamlCodec', 'register_yamlable_codec', 'yaml_info_decorate', 'yaml_info', 'YamlAble', 'YA',
+           'AbstractYamlAble',
+           'YAMLABLE_PREFIX', 'YamlObject2', 'ABCYAMLMeta', 'YAMLObjectMetaclassStrict'
+           ]
